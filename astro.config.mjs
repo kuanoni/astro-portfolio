72810a24e4a4150029b3603217041b1f0f80d372
kuanoni <tehgamemaker@gmail.com>
2023-01-30 11:28:04 -0800

    add sass math module import

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-import image from "@astrojs/image";
+import image from '@astrojs/image';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -11,25 +11,26 @@ const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), image()],
-  vite: {
-    resolve: {
-      alias: {
-        '@/': `${path.resolve(__dirname, 'src')}/`
-      }
-    },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          // path to your scss variables
-          additionalData: `
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap(), image()],
+	vite: {
+		resolve: {
+			alias: {
+				'@/': `${path.resolve(__dirname, 'src')}/`,
+			},
+		},
+		css: {
+			preprocessorOptions: {
+				scss: {
+					// path to your scss variables
+					additionalData: `
+                    @use 'sass:math';
                     @import "@/styles/abstracts/_variables.scss"; 
                     @import "@/styles/abstracts/_functions.scss"; 
                     @import "@/styles/abstracts/_mixins.scss";
-                    `
-        }
-      }
-    }
-  }
+                    `,
+				},
+			},
+		},
+	},
 });
