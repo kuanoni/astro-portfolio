fd53e46c2c5974a051e53f86d323de054557aad2
kuanoni <tehgamemaker@gmail.com>
2023-01-31 14:00:28 -0800

    add aboveMiddle flag

--- src/utils/MiddleIoManager.ts
+++ src/utils/MiddleIoManager.ts
@@ -2,12 +2,18 @@ import IoManager from './IoManager';
 
 interface iFlags {
 	middle: boolean;
+	aboveMiddle: boolean;
 }
 
 const MiddleIoManager = new IoManager<iFlags>(
 	(entries) => {
 		const entry = entries[0];
-		return { middle: entry.isIntersecting };
+		const rootRect = entry.rootBounds;
+		const elRect = entry.boundingClientRect;
+
+		if (!rootRect) throw new Error(`IntersectionObserver entry has null root: ${entry}`);
+
+		return { middle: entry.isIntersecting, aboveMiddle: elRect.bottom < rootRect.bottom - rootRect.height / 2 };
 	},
 	{ rootMargin: '-50% 0% -50% 0%' }
 );
