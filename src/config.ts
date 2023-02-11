c02bf738a5e8894dc060e290ff1d240e187f2763
kuanoni <tehgamemaker@gmail.com>
2023-02-11 09:47:08 -0800

    add social media urls to config

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,9 @@
 
 export const SITE_TITLE = 'Kainoa Araiza K.';
 export const SITE_DESCRIPTION = 'Welcome to my portfolio!';
+
+export const GITHUB_URL = 'https://github.com/kuanoni';
+export const GITLAB_URL = 'https://gitlab.com/kuanoni';
+export const LINKEDIN_URL = '';
+export const STACK_OVERFLOW_URL = 'https://stackoverflow.com/users/20453324/kuanoni';
+export const DEV_URL = 'https://dev.to/kuanoni';
