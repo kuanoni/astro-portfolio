fc501d819e2b3f5de3d51d338776dd579c0442bc
kuanoni <tehgamemaker@gmail.com>
2023-02-19 09:57:18 -0800

    add TECH_INFO

--- src/config.ts
+++ src/config.ts
@@ -9,3 +9,49 @@ export const GITLAB_URL = 'https://gitlab.com/kuanoni';
 export const LINKEDIN_URL = '';
 export const STACK_OVERFLOW_URL = 'https://stackoverflow.com/users/20453324/kuanoni';
 export const DEV_URL = 'https://dev.to/kuanoni';
+
+interface TechInfo {
+	[i: string]: {
+		description: string;
+		url: string;
+	};
+}
+
+export const TECH_INFO: TechInfo = {
+	Javascript: {
+		description: 'You probably know what Javascript is...',
+		url: 'https://en.wikipedia.org/wiki/JavaScript',
+	},
+	Typescript: {
+		description: 'A strongly typed version of Javascript.',
+		url: 'https://www.typescriptlang.org/',
+	},
+	React: {
+		description: 'A popular Javascript library for building user interfaces. My go-to.',
+		url: 'https://reactjs.org/',
+	},
+	Next: {
+		description: 'An full-stack web framework that extends on React.',
+		url: 'https://nextjs.org/',
+	},
+	Stiches: {
+		description: 'CSS-in-JS with near-zero runtime, SSR, and multi-variant support.',
+		url: 'https://stitches.dev/',
+	},
+	MongoDB: {
+		description: "Document-oriented database program. It's easy to use, and they provide hosting.",
+		url: 'https://www.mongodb.com/',
+	},
+	Sass: {
+		description: 'Popular CSS extension language. My go-to...until W3C adds nesting to CSS.',
+		url: 'https://sass-lang.com/',
+	},
+	Figma: {
+		description: 'Powerful interface design tool.',
+		url: 'https://www.figma.com/',
+	},
+	Express: {
+		description: 'Node.js framework for creating web servers.',
+		url: 'https://expressjs.com/',
+	},
+};
