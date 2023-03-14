431dd0bf6bcec0d6a0a35e0f05f6383c5127b7dd
kuanoni <tehgamemaker@gmail.com>
2023-03-14 09:48:36 -0700

    change site description

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,5 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'kainoa.us';
-export const SITE_DESCRIPTION = 'Welcome to my portfolio!';
+export const SITE_DESCRIPTION = 'Kainoa Araiza Kuanoni: Front-end web developer, personal website and portfolio.';
 
 export const GITHUB_URL = 'https://github.com/kuanoni';
 export const GITLAB_URL = 'https://gitlab.com/kuanoni';
